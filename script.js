4478255cefe1f512926fc418921892997e7ae054
uendi <uendipici71@gmail.com>
2024-07-19 14:34:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const $next = document.querySelector('.next');
+const $prev = document.querySelector('.prev');
+
+$next.addEventListener (
+    'click',
+    () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+
+    },
+);
+
+$prev.addEventListener (
+    'click',
+    () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(itemss[items.length -1]);
+
+    }, 
+);
